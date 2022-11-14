78cc92872a90890d5bd6405880741e0c882cf5b1
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-11-14 19:49:24 -0300

    Criação de teste para a classe UserServiceImpl, método create com sucesso

--- src/test/java/br/com/anderson/api/services/implementations/UserServiceImplTest.java
+++ src/test/java/br/com/anderson/api/services/implementations/UserServiceImplTest.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -92,7 +93,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        when(userRepository.save(any())).thenReturn(usuario);
+
+        Usuario response = userService.create(usuarioDTO);
+
+        assertNotNull(response);
+        assertEquals(Usuario.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
