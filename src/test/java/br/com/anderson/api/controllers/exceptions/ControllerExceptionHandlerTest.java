ea1d55480241976d70df4b6ba294970feac59928
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-11-19 22:04:23 -0300

    Criação de teste para a classe ControllerExceptionHandler, método object not found implementado

---
+++ src/test/java/br/com/anderson/api/controllers/exceptions/ControllerExceptionHandlerTest.java
@@ -0,0 +1,44 @@
+package br.com.anderson.api.controllers.exceptions;
+
+import br.com.anderson.api.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ControllerExceptionHandlerTest {
+
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    @InjectMocks
+    private ControllerExceptionHandler controllerExceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void WhenObjectNotFoundExceptionThenReturnAResponseEntity() {
+        ResponseEntity<StandardError> response = controllerExceptionHandler.objectNotFound(
+                new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO),
+                new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(OBJETO_NAO_ENCONTRADO, response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+    }
+
+    @Test
+    void dataIntegratyViolationException() {
+    }
+}
